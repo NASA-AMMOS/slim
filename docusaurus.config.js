68a253662b3af2fbae4f6345f7b3d669e05de9d1
Kyongsik Yun <yunkss@gmail.com>
2024-06-26 13:46:06 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   organizationName: 'nasa-ammos', // Usually your GitHub org/user name.
   projectName: 'slim', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
